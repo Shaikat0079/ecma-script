2df9d775e5e043d9db09ec1cc740fd641584e5d5
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-06 15:04:43 +0930

    destruturing

---
+++ destructuring.js
@@ -0,0 +1,21 @@
+const hena = {
+    pName : "Hena",
+    age : 10,
+    friends : ['hero-alm',"bappa","virat","sachin"],
+    pStatus: "Not Found"
+}
+
+const {pName,age,pStatus,friends}=hena
+console.log(pName)
+console.log(friends)
+console.log(pStatus)
+
+
+const person = ['hero alm','Arafat',"tauhid",'Sami Azam',"Charles  Yeo"];
+
+const [nayok, junior, classMate,teacher,professor] = person;
+console.log(nayok)
+console.log(junior);
+console.log(classMate)
+console.log(teacher)
+console.log(professor)
