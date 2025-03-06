560f9200458a8db552cb2bb8fa35cfcde4c4f028
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-06 13:18:13 +0930

    using spread operator

---
+++ spread.js
@@ -0,0 +1,7 @@
+const character = ["optimus",'Megatron','Stars Scream','Ultron'];
+console.log(...character)
+
+const newArray = ["Iron Man",character]
+console.log(newArray)
+const newArray2 = ["Iron Man",...character]
+console.log(newArray2)
