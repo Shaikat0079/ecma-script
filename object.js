ef2bfc6c7d648a15011d756dfaec1ca71fedbd13
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-08 03:31:46 +0930

    Added some more

--- object.js
+++ object.js
@@ -3,6 +3,7 @@ const person = {
     age : 10,
     country: "BD",
 }
+// delete person.pName;
 Object.freeze(person)
 // Object.seal(person)
 person.age+=10;
@@ -10,3 +11,26 @@ person.status="not found"
 console.log(Object.keys(person))
 console.log(Object.values(person))
 console.log(Object.entries(person))
+
+for (const key in person) {
+    if (Object.prototype.hasOwnProperty.call(person, key)) {
+        const element = person[key];
+        console.log(element)
+        
+    }
+}
+
+for (let key in person)
+{
+    console.log(key)
+}
+
+for (let key in person){
+    console.log(`key : ${key} value : ${person[key]}`)
+}
+
+console.log(Object.entries(person))
+
+for (let [key,value]of Object.entries(person)){
+    console.log(`key : ${key} value :  ${value}`)
+}
